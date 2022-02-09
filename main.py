07485eaec8c805fbba6e76cbd57bf2c68e3afeb2
Matt <mlee138@binghamton.edu>
2022-02-08 23:03:30 -0500

    added checklist function

--- main.py
+++ main.py
@@ -9,6 +9,50 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+checklists = {}
+
+
+def handle_checklist(message, author):
+    _, action, *rest = message.split(" ")
+    value = " ".join(rest) if len(rest) >= 0 else ""
+
+    if author not in checklists:
+        checklists[author] = []
+
+    if action == "help":
+        return """```AVAILABLE ACTIONS
+        view
+        add <item>
+        completed <item number>
+        delete <item number>
+        clear 
+        ```"""
+    elif action == "view":
+        if len(checklists[author]) == 0:
+            return "```no items in your checklist```"
+        list_str = ""
+        for i in range(len(checklists[author])):
+            list_str += f'{i}. {checklists[author][i]}\n'
+        # list_str = f'```{list_str}```'
+        return list_str
+    elif action == "add" and value:
+        checklists[author].append(value)
+        return "```successfully added to checklist```"
+    elif action == "complete" and value:
+        checklists[author][int(value)] = f'~~{checklists[author][int(value)]}~~'
+        return "```item has been marked as completed```"
+    # elif action == "" and value:
+    #     checklists[author][value] = f'~~${checklists[author][value]}~~'
+    #     return "item has been marked as completed"
+    elif action == "delete" and value:
+        checklists[author].remove(checklists[author][int(value)])
+        return "```deleted item from checklist```"
+    elif action == "clear":
+        checklists[author] = []
+        return "```cleared checklist```"
+    else:
+        return f'{action} is not recognized or was used incorrectly'
+
 
 # on_ready: called when the bot is started
 @client.event
@@ -18,11 +62,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    author = str(message.author)
     username = str(message.author).split("#")[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
 
+    # if the bot sent the message, return
     if message.author == client.user:
         return
     if message.channel.name == 'discord-bot-channel':
@@ -34,6 +80,10 @@ async def on_message(message):
             return
         elif user_message.lower() == 'random':
             response = f'random number: {random.randrange(1000000)}'
+            await message.channel.send(response)
+            return
+        elif user_message.split(" ")[0] == '!checklist':
+            response = handle_checklist(user_message, author)
             await message.channel.send(response)
             return
 
